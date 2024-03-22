c4f9ee59f1eebb83041439fb556f46de7de38a75
Manish Jha <manishjha962@gmail.com>
2024-03-22 20:37:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+const add = document.querySelector(".btn")
+var scr = document.getElementById("scr")
+let container = document.getElementById("list-container")
+
+add.addEventListener("click", () => {
+    if (scr.value === "") {
+        alert("Add your task first")
+    }
+    else {
+        let li = document.createElement("li")
+        li.innerHTML = scr.value
+        container.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "&#x274C"
+        li.append(span)
+        clear.style.display = "flex"
+        saveData()
+    }
+    scr.value = ""
+    saveData()
+})
+scr.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        if (scr.value === "") {
+            alert("Add your task first")
+        }
+        else {
+            let li = document.createElement("li")
+            li.innerHTML = scr.value
+            container.appendChild(li)
+
+            let span = document.createElement("span")
+            span.innerHTML = "&#x274C"
+            li.append(span)
+            clear.style.display = "flex"
+            saveData()
+        }
+        scr.value = ""
+        saveData()
+    }
+})
+
+container.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("check")
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+let clear = document.getElementById("btn")
+let lis = document.querySelectorAll("li")
+
+clear.addEventListener("click", ()=>{
+    container.innerHTML = ""
+    clear.style.display = "none"
+    saveData().remove()
+})
+function saveData() {
+    localStorage.setItem("data", container.innerHTML)
+}
+
+function showList() {
+    container.innerHTML = localStorage.getItem("data")
+}
+
+
+showList()
